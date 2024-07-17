bc0db4e554ee374fbc34103a88c4398d6e638fb9
Istiak <mhistiak2@gmail.com>
2024-07-17 11:24:59 +0600

    Basic application setup and database connection completed

---
+++ index.js
@@ -0,0 +1,35 @@
+// dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+// application module 
+const crudHandler = require("./routes/crudHandler");
+
+// express app init
+const app = express();
+// application middleware
+app.use(express.json());
+
+// database connection with mongoose
+mongoose
+  .connect("mongodb://localhost/CRUD")
+  .then(() => console.log("connect succesfully"))
+  .catch((err) => console.log(err))
+
+// Application Routes middleware
+app.use("/crud", crudHandler);
+app.get("/", (req, res) => {
+  res.json({ name: "Mongoose CRUD Application" });
+});
+
+// Handle Error
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500).json({ error: err.message });
+});
+
+// Server listen
+app.listen(300, () => {
+  console.log(`Server listening: http://localhost:300`);
+});
---
+++ routes/crudHandler.js
@@ -0,0 +1,30 @@
+// dependencies
+const express = require("express");
+const router = express.Router()
+
+// read all data
+router.get("/",async (req,res)=>{
+    
+})
+// read single data by id
+router.get("/:id",async (req,res)=>{
+
+})
+// create data
+router.post("/",async (req,res)=>{
+
+})
+// create mutiple data
+router.post("/all",async (req,res)=>{
+
+})
+// update data
+router.put("/",async (req,res)=>{
+
+})
+// delete single data by id
+router.delete("/:id",async (req,res)=>{
+
+})
+
+module.exports = router
